36ad146a938ed46be635993b7a6a4b39c1c8b20a
Ben Bauer <ben.bauer1@gmail.com>
2021-07-26 15:21:45 -0500

    question text is displayed

--- ui.py
+++ ui.py
@@ -1,8 +1,10 @@
 THEME_COLOR = "#375362"
-from tkinter import Canvas, Label, PhotoImage, Tk
+from tkinter import Canvas, Label, PhotoImage, Tk, Button
+from quiz_brain import QuizBrain
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title(("Trivia!"))
         self.window.config(bg=THEME_COLOR, width=360, height=500)
@@ -14,17 +16,21 @@ class QuizInterface:
 
         self.canvas = Canvas()
         self.canvas.config(width=300, height=250, bg="white")
-        self.canvas.grid(row=1, column=0, columnspan=2)
+        self.question_text = self.canvas.create_text(150,125, text="Some Text", fill=THEME_COLOR, font=("Arial", 20, "italic"), width=275)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
-        self.true_img = Canvas(width=100, height=100)
-        img = PhotoImage("images/true.png")
-        self.true_img.create_image(0,0, image=img)
+        img_true = PhotoImage(file="images/true.png")
+        self.true_img = Button(image=img_true, highlightthickness=0)
         self.true_img.grid(row=2,column=0)
 
-        self.false_img = Canvas(width=100, height=100)
-        img = PhotoImage("images/false.png")
-        self.true_img.create_image(0,0, image=img)
-        self.true_img.grid(row=2,column=1)
 
+        img_false = PhotoImage(file="images/false.png")
+        self.false_img = Button(image=img_false, highlightthickness=0)
+        self.false_img.grid(row=2,column=1)
 
+        self.getNextQuestion()
         self.window.mainloop()
+
+    def getNextQuestion(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)

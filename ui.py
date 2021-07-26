ee5173cd1ebbfc3a6f72f9e6f43d37a7f11ad0a2
Ben Bauer <ben.bauer1@gmail.com>
2021-07-26 16:34:43 -0500

    score keeping working, flips through 10 questions

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 THEME_COLOR = "#375362"
 from tkinter import Canvas, Label, PhotoImage, Tk, Button
+from tkinter.constants import S
 from quiz_brain import QuizBrain
 
 class QuizInterface:
@@ -20,17 +21,39 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         img_true = PhotoImage(file="images/true.png")
-        self.true_img = Button(image=img_true, highlightthickness=0)
+        self.true_img = Button(image=img_true, highlightthickness=0, command=self.true_pressed)
         self.true_img.grid(row=2,column=0)
 
 
         img_false = PhotoImage(file="images/false.png")
-        self.false_img = Button(image=img_false, highlightthickness=0)
+        self.false_img = Button(image=img_false, highlightthickness=0,command=self.false_pressed)
         self.false_img.grid(row=2,column=1)
 
         self.getNextQuestion()
         self.window.mainloop()
 
     def getNextQuestion(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You have reached the end of the game!")
+            self.true_img.config(state="disabled")
+            self.false_img.config(state="disabled")
+
+    def true_pressed(self):
+        print("true")
+        self.checkAnswer("True")
+
+    def false_pressed(self):
+        self.checkAnswer("False")
+
+    def checkAnswer(self, answer:str):
+        isRight = self.quiz.check_answer(answer)
+        if(isRight):
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.getNextQuestion)
